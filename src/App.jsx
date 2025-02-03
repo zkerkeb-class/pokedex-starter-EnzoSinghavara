89b665b9c571dae9a3353c94880b57289afcd9e9
yellow_enzo <enzo.singhavara@edu.ece.fr>
2025-02-03 18:00:45 +0100

    header add

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-//import Header from './src/index.jsx'
+import Header from './components/header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+        <Header />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,7 +2,7 @@
 const Header = () => {
     return (
         <div>
-            <h1>Bonjour</h1>
+            <h1>Bonjour, bienvenue au Pokédex</h1>
         </div>
     )
 }
